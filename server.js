c22a52c81bfac9f7efa72f2175da704c6b5276b6
moradisten <moradabou1996@gmail.com>
2021-05-02 22:55:21 +0200

    conf server

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const path = require('path');
-const history = require('connect-history-api-fallback');
 
 const app = express();
 
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 app.use(staticFileMiddleware);
-app.use(history({
-    disableDotRule: true,
-    verbose: true
-}));
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
