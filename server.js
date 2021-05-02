87906bce3d04c61c128c2f1dbd7bb203197c0a9f
moradisten <moradabou1996@gmail.com>
2021-05-02 22:50:49 +0200

    conf server

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get('/', function (req, res) {
     res.render(path.join(__dirname + '/dist/index.html'));
 });
 
+
+
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
